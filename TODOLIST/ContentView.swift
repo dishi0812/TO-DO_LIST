7266aadeb5e7c01624f3f49af7b7c9282fa9660b
dishita <dishita.bharadia@gmail.com>
2022-06-25 10:15:30 +0800

    Initial Commit

---
+++ TODOLIST/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TODOLIST
+//
+//  Created by T Krobot on 25/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TODOLIST/TODOLISTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOLISTApp.swift
+//  TODOLIST
+//
+//  Created by T Krobot on 25/6/22.
+//
+
+import SwiftUI
+
+@main
+struct TODOLISTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
